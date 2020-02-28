6aee42b595e30cc5f12abf7d602dd1c2e0df2307
luwenchun <luwenchun@users.noreply.github.com>
2020-02-28 10:54:09 +0800

    code 1

---
+++ flutter_widget_demo/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mac/flutterdevelopment/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mac/code/flutter/The-Guide-to-the-Flutter-master/flutter_widget_demo"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mac/flutterdevelopment/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
